e9ff45d90fd0855e460494cba7cb4e8893822923
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-06-14 23:20:18 +0800

    Updated part three SW

--- SW1-AngielynRivera/PartThree.cs
+++ SW1-AngielynRivera/PartThree.cs
@@ -10,7 +10,32 @@ namespace SW1_AngielynRivera
     {
         public static void PartThreeSW()
         {
-            
+            string result = "";
+            int x = 3;
+
+            while (x > 0)
+            {
+                if (x > 2)
+                {
+                    result = result + "a";
+                }
+
+                if (x == 2)
+                {
+                    result = result + "b c";                
+                }
+
+                x = x - 1;
+                result = result + "-";
+
+                if (x == 1)
+                {
+                    result = result + "d";
+                    x = x - 1;
+                }   
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
