6261f6239a64b328f33337cd52d38c90a754261e
kevs de guzman <kevinpacunayen@gmail.com>
2022-06-14 13:02:53 +0800

    1st SW

---
+++ Module02/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1
+{
+    internal class Output
+    {
+        internal static void Text()
+        {
+
+            string result = "";
+
+            int x = 3;
+
+
+            while (x > 0)
+            {
+
+
+                if (x > 2)
+                {
+                    result = result + "a";
+
+                }
+
+
+                if (x == 2)
+                {
+                    result = result + "b c";
+
+                }
+
+
+                if (x == 1)
+                {
+
+                    result = result + "d";
+                    x = x - 1;
+
+
+                }
+
+
+
+                if (x > 0)
+                {
+                    x = x - 1;
+                    result = result + "-";
+                }
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
--- Module02/Program.cs
+++ Module02/Program.cs
@@ -1,31 +1,13 @@
-﻿namespace Module02
+﻿using SW1;
+namespace Module02
 {
     public class Program
     {
         private static void Main()
         {
-            //BonusCalculator.BonusCalculatorNestedIf();
-            //CityPicker.SwitchCaseSampleCode();
-            //LargestValue.LargestValueDemoCode();
 
-            //WhileLoop.InputValuesLoop();
-            //ForLoop.InputValuesLoop();
-            //ForEachLoop.ForEachLoopSampleCode();
-            DoWhileLoop.InputValuesLoop();
-
-            //int aValue = 100, bValue = 1000;
-            //string sValue = "CS158";
-            //decimal money = 50.22m;
-            //double dValue = 50.22;
-            //char cValue = 'A';
-
-
-            //bool result = sValue < "CS";
-            //bool result1 = sValue > "CS";
-            //bool result2 = sValue == "CS";
-            //bool result3 = sValue >= "CS";
-
-            //bool res = (true && false) || false;
+            Output.Text();
+  
         }
     }
 }
