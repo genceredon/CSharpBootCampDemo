73d504f16195cf26dfbf584ea39004a2a3b595af
genceredon <gjgceredon@gmail.com>
2022-06-16 02:02:38 +0800

    Added project for Module05

---
+++ Module05/ArrayUsingForEach.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module05
+{
+    internal partial class ArraySampleCode
+    {
+        public void UsingForEach()
+        {
+            string[] color = { "red", "green", "blue" };
+            foreach (string val in color)
+            {
+                Console.WriteLine(val);
+            }
+
+        }
+    }
+}
---
+++ Module05/PassingArray.cs
@@ -0,0 +1,27 @@
+﻿namespace Module05
+{
+    internal partial class ArraySampleCode
+    {
+        public void PassingArray()
+        {
+            int[] temperature = new int[5];
+            int[] t = new int[5];
+            InputValues(temperature);
+
+            //Array t will reference the same array as the temperature array
+            t = temperature;
+            t[1] = 44;
+            Console.WriteLine(temperature[1]);
+        }
+
+        private static void InputValues(int[] temp)
+        {
+            for (int i = 0; i < temp.Length; i++)
+            {
+                Console.Write("Enter Temperature {0}: ", i + 1);
+                string? inValue = Console.ReadLine();
+                temp[i] = int.Parse(inValue);
+            }
+        }
+    }
+}
