f2c09bf288e1dbe9a01ea4a8b4bdf63ac551bc58
epmoncayo <epmoncayo@hotmail.com>
2022-06-15 19:14:31 +0800

    Initial solution (#1)
    Co-authored-by: Edwin Moncayo <933788@cognizant.com>

---
+++ ExerciseSolution/AngielynRivera/Bootcamp/Bootcamp.Calculation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Bootcamp.Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            int result;
+
+            Console.WriteLine("Calculate Numbers. Enter 2 number and we return result");
+            Console.WriteLine("Enter 1st Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            result = Add(num1, num2);
+            Console.WriteLine("Add : " + result.ToString());
+            Console.ReadLine();
+        }
+
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public static int Subtract(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public static int Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public static int Divide(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+    }
+}
