75d41076043b76c4d60ba8fd6fbd28344ea4039a
miguelrefuerzo <mlmrefuerzo@gmail.com>
2022-06-16 14:10:56 +0800

    Added while to loop the process Added switch case for user input

--- ExerciseSolution/MiguelRefuerzo/Bootcamp/Bootcamp.Calculation/Program.cs
+++ ExerciseSolution/MiguelRefuerzo/Bootcamp/Bootcamp.Calculation/Program.cs
@@ -8,16 +8,64 @@ namespace Bootcamp.Calculation
         {
             int num1, num2;
             int result;
-            var test = 0;
 
-            Console.WriteLine("Calculate Numbers. Enter 2 number and we return result");
-            Console.WriteLine("Enter 1st Number: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 2nd Number: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            result = Add(num1, num2);
-            Console.WriteLine("Add : " + result.ToString());
-            Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Do you like to: \na. Add\nb. Subtract\nc. Multiply\nd. Divide\ne. Exit");
+                string choice = Console.ReadLine();
+
+                switch (choice.ToLower())
+                {
+                    case "a":
+                        Console.Clear();
+                        Console.WriteLine("Calculate Numbers. Enter 2 number and we return result");
+                        Console.WriteLine("Enter 1st Number: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter 2nd Number: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        result = Add(num1, num2);
+                        Console.WriteLine("Add : " + result.ToString());
+                        Console.ReadLine();
+                        break;
+                    case "b":
+                        Console.Clear();
+                        Console.WriteLine("Calculate Numbers. Enter 2 number and we return result");
+                        Console.WriteLine("Enter 1st Number: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter 2nd Number: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        result = Subtract(num1, num2);
+                        Console.WriteLine("Add : " + result.ToString());
+                        Console.ReadLine();
+                        break;
+                    case "c":
+                        Console.Clear();
+                        Console.WriteLine("Calculate Numbers. Enter 2 number and we return result");
+                        Console.WriteLine("Enter 1st Number: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter 2nd Number: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        result = Multiply(num1, num2);
+                        Console.WriteLine("Add : " + result.ToString());
+                        Console.ReadLine();
+                        break;
+                    case "d":
+                        Console.Clear();
+                        Console.WriteLine("Calculate Numbers. Enter 2 number and we return result");
+                        Console.WriteLine("Enter 1st Number: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter 2nd Number: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        result = Divide(num1, num2);
+                        Console.WriteLine("Add : " + result.ToString());
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            
         }
 
         public static int Add(int firstNumber, int secondNumber)
@@ -37,7 +85,7 @@ namespace Bootcamp.Calculation
 
         public static int Divide(int firstNumber, int secondNumber)
         {
-            return firstNumber * secondNumber;
+            return firstNumber / secondNumber;
         }
     }
 }
